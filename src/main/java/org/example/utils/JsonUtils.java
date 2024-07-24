a03962a60311901bf1b4b14e256a2192fde0866e
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-07-24 21:12:22 +0700

    test json dataprovider

--- src/main/java/org/example/utils/JsonUtils.java
+++ src/main/java/org/example/utils/JsonUtils.java
@@ -1,32 +1,14 @@
-package org.example.tests;
+package org.example.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.assertj.core.api.Assertions;
-
-import org.example.pages.HomePage;
-import org.example.pages.LoginPage;
-
-import org.example.utils.ExcelUtils;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
-public class LoginPageTest extends BaseTest{
-
-    private LoginPageTest(){}
-
-    @Test(dataProvider = "getData")
-    public void test1(Map<String,Object> map) {
-        map.forEach((k,v)->System.out.println(k+":"+v));
-    }
+public class JsonUtils {
 
     @DataProvider
     public Object[] getData() throws IOException {
@@ -39,5 +21,4 @@ public class LoginPageTest extends BaseTest{
 
         return new Object[]{map1, map2};
     }
-
 }
